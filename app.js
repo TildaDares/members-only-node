1ba2dab86f40ca4e34fce49c331e744d4dc0f986
TildaDares <mathildaudufo@gmail.com>
2022-03-09 12:07:47 +0100

    add express-validator, helmet, compression packages

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var helmet = require("helmet");
+var compression = require("compression");
 require("dotenv").config();
 
 var indexRouter = require("./routes/index");
@@ -22,6 +24,8 @@ db.on("error", console.error.bind(console, "MongoDB connection error:"));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(helmet());
+app.use(compression());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
