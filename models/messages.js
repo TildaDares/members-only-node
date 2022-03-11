33814b061cbf951f9a920139858910d09a62a9c6
TildaDares <mathildaudufo@gmail.com>
2022-03-11 22:43:45 +0100

    create user and message model

---
+++ models/messages.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const MessageSchema = new Schema({
+  content: { type: String, required: true },
+  created_at: { type: Date, required: true },
+  user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+});
+
+MessageSchema.virtual("created_at_formatted").get(function () {
+  return DateTime.fromJSDate(this.created_at).toLocaleString(
+    DateTime.DATETIME_MED
+  );
+});
+module.exports = mongoose.model("Message", MessageSchema);
---
+++ models/user.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  first_name: { type: String, require: true },
+  last_name: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  membership_status: {
+    type: String,
+    default: "Normal",
+    enum: ["Normal", "Elite"],
+  },
+  isAdmin: { type: Boolean, default: false },
+});
+
+UserSchema.virtual("fullname").get(function () {
+  return this.first_name + " " + this.last_name;
+});
+
+module.exports = mongoose.model("User", UserSchema);
