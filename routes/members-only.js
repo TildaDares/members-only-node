dce4220b0645951aa84dedc682e0a7c879d1a5b1
TildaDares <mathildaudufo@gmail.com>
2022-03-12 00:53:23 +0100

    routes

---
+++ routes/members-only.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var router = express.Router();
+var messagesController = require("../controllers/messagesController");
+var usersController = require("../controllers/usersController");
+
+/* GET home page. */
+router.get("/", messagesController.index);
+
+/* Session routes */
+router.get("/sign-up", usersController.signUpGet);
+router.post("/sign-up", usersController.signUpPost);
+router.post("/login", usersController.loginPost);
+router.get("/login", usersController.loginGet);
+router.get("/logout", usersController.logout);
+
+module.exports = router;
