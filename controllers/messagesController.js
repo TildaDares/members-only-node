44eef1ef505016efd9bfd2fb0e444b94c0e10b2c
TildaDares <mathildaudufo@gmail.com>
2022-03-12 00:53:14 +0100

    created messages and users controllers

---
+++ controllers/messagesController.js
@@ -0,0 +1,45 @@
+const Message = require("../models/messages");
+const { body, validationResult } = require("express-validator");
+
+exports.index = function (req, res, next) {
+  Message.find({})
+    .populate("user")
+    .exec(function (err, results) {
+      if (err) {
+        const error = new Error(err);
+        next(error);
+      }
+
+      res.render("index", { messages: results });
+    });
+};
+
+exports.new = [
+  body("content")
+    .trim()
+    .isLength({ min: 1, max: 360 })
+    .escape()
+    .withMessage("Message content cannot be empty"),
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.render("new.ejs");
+      return;
+    }
+
+    let message = new Message({
+      content: req.body.content,
+      user: req.user._id,
+      created_at: new Date(),
+    });
+
+    message.save(function (err, result) {
+      if (err) {
+        return next(err);
+      }
+
+      res.redirect("/");
+    });
+  },
+];
---
+++ controllers/usersController.js
@@ -0,0 +1,37 @@
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
+const passport = require("passport");
+
+exports.signUpGet = function (req, res, next) {
+  res.render("users/sign-up");
+};
+
+exports.signUpPost = function (req, res, next) {
+  bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
+    // if err, do something
+    if (err) return next(err);
+    // otherwise, store hashedPassword in DB
+    const user = new User({
+      username: req.body.username,
+      password: hashedPassword,
+    }).save((err) => {
+      if (err) return next(err);
+
+      res.redirect("/");
+    });
+  });
+};
+
+exports.loginGet = function (req, res, next) {
+  res.render("users/sign-in");
+};
+
+exports.loginPost = passport.authenticate("local", {
+  successRedirect: "/",
+  failureRedirect: "/",
+});
+
+exports.logout = function (req, res, next) {
+  req.logout();
+  res.redirect("/");
+};
