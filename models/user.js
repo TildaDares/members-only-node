699e90b4610aa5921a8958d0f08ac7d1db800dba
TildaDares <mathildaudufo@gmail.com>
2022-03-12 00:12:48 +0100

    remove first_name and last_name fields

--- models/user.js
+++ models/user.js
@@ -2,9 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  first_name: { type: String, require: true },
-  last_name: { type: String, required: true },
-  email: { type: String, required: true },
+  username: { type: String, required: true },
   password: { type: String, required: true },
   membership_status: {
     type: String,
@@ -12,10 +10,6 @@ const UserSchema = new Schema({
     enum: ["Normal", "Elite"],
   },
   isAdmin: { type: Boolean, default: false },
-});
-
-UserSchema.virtual("fullname").get(function () {
-  return this.first_name + " " + this.last_name;
 });
 
 module.exports = mongoose.model("User", UserSchema);
